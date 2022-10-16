0017895b3b9e1bab4d802b8083e9a375d5245018
David Viuche <jviucheroja@uniminuto.edu.co>
2022-10-16 12:45:52 -0500

    fix mobile input size

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -2,7 +2,7 @@ const Input = ({ name, type, label, value, handleOnchage, handleOnInput }) => {
     return (
         <div className="flex flex-col w-full">
             <label htmlFor={name} className='text-sm text-zinc-400'>{label}</label>
-            <input type={type} name={name} id={name} value={value} onChange={handleOnchage} onInput={handleOnInput} className='p-3 mb-5 shadow-md border-2 rounded-md bg-white'/>
+            <input type={type} name={name} id={name} value={value} onChange={handleOnchage} onInput={handleOnInput} className='p-3 w-full mb-5 shadow-md border-2 rounded-md bg-white'/>
         </div>
     )
 }
